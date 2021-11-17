bb25c4795a7d20f7af28a8d8f248dbf60873dcc8
NancyWachiuri <nancywachiuri@gmail.com>
2021-11-17 19:49:00 +0300

    Push to branch

--- src/app/services/authapi.service.ts
+++ src/app/services/authapi.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthapiService {
 
-  constructor() { }
+  // constructor() { private http: HttpClient}
+  // url= "https://clinicaappcci.herokuapp.com/app/api";
+
+
 }
