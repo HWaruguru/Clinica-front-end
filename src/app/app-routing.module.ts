6ea1179204d8ef66dc262b3f6d8fb2d8f5789f43
NancyWachiuri <nancywachiuri@gmail.com>
2021-11-18 15:19:54 +0300

    Deploy to github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {RegisterComponent} from './components/register/register.component';
 import {AboutComponent} from './components/about/about.component';
 import {ContactComponent} from './components/contact/contact.component';
 import { TeamComponent } from './components/team/team.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -14,6 +15,7 @@ const routes: Routes = [
   {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent},
   {path:'team', component: TeamComponent},
+  {path: 'landing', component: LandingComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
