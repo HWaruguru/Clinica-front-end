346be95a17d3f98956f3184a5599184d7a0c2bbb
HWaruguru <hannah.waruguru8@gmasil.com>
2021-11-19 10:25:34 +0300

    add landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,17 @@ import { LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
 import {AboutComponent} from './components/about/about.component';
 import {ContactComponent} from './components/contact/contact.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 const routes: Routes = [
+  { path: 'index', component: LandingComponent },
   { path: 'home', component: HomeComponent },
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
   {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent},
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: '**', redirectTo: 'home', pathMatch: 'full'}
+  {path: '', redirectTo: 'index', pathMatch: 'full'},
+  {path: '**', redirectTo: 'index', pathMatch: 'full'}
 ];
 
 
